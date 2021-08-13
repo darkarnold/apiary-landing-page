05199c44a3228e727712f90bcefb289c3a7e03e9
SilentStorm <tongkorn.p@gmail.com>
2021-08-13 15:58:24 +0700

    Feat: Add main landing section

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import Main from './Main'
+
+function App() {
+
+    return (
+        <div class="root">
+            <div class="root__container">
+                <Main/>
+            </div>
+        </div>
+    )
+}
+
+export default App;
