13bbf358d4a8a8e133544b6d96907054a88b3959
SilentStorm <tongkorn.p@gmail.com>
2021-08-17 23:53:35 +0700

    Fix: add data.js, fix css

--- src/components/ContentWhy.js
+++ src/components/ContentWhy.js
@@ -8,19 +8,19 @@ function contentWhy(props) {
                 <h3 className="content__text content__text_theme_dark">{props.content.subtitle}</h3>
                 <div className="grid grid__items">
                     <div className="grid__item">
-                        <img src={`${props.content.topItemImage}`} alt="resource saving" className="grid__item-image" />
-                        <h4 className="grid__title">{props.content.topItemTitle}</h4>
-                        <p className="grid__subtitle">{props.content.topItemSubtitle}</p>
+                        <img src={`${props.content.topItem.image}`} alt={props.content.topItem.title} className="grid__item-image" />
+                        <h4 className="grid__title">{props.content.topItem.title}</h4>
+                        <p className="grid__subtitle">{props.content.topItem.subtitle}</p>
                     </div>
                     <div className="grid__item">
-                        <img src={`${props.content.leftItemImage}`} alt="publicize" className="grid__item-image" />
-                        <h4 className="grid__title">{props.content.leftItemTitle}</h4>
-                        <p className="grid__subtitle">{props.content.leftItemSubtitle}</p>
+                        <img src={`${props.content.leftItem.image}`} alt={props.content.leftItem.title} className="grid__item-image" />
+                        <h4 className="grid__title">{props.content.leftItem.title}</h4>
+                        <p className="grid__subtitle">{props.content.leftItem.subtitle}</p>
                     </div>
                     <div className="grid__item">
-                        <img src={`${props.content.rightItemImage}`} alt="PR boost" className="grid__item-image" />
-                        <h4 className="grid__title">{props.content.rightItemTitle}</h4>
-                        <p className="grid__subtitle">{props.content.rightItemSubtitle}</p>
+                        <img src={`${props.content.rightItem.image}`} alt={props.content.rightItem.title} className="grid__item-image" />
+                        <h4 className="grid__title">{props.content.rightItem.title}</h4>
+                        <p className="grid__subtitle">{props.content.rightItem.subtitle}</p>
                     </div>
                 </div>
             </div>
--- src/components/SectionWhyUseful.js
+++ src/components/SectionWhyUseful.js
@@ -1,23 +1,8 @@
 import React from "react";
 import ContentWhy from "./ContentWhy";
-import prBoost from "../images/PR-boost.png";
-import resourceSaving from "../images/resource-saving.png";
-import publicize from "../images/publicize.png";
+import contentWhyUseful from "../data";
 
 function SectionWhyUseful() {
-    const contentWhyUseful = {
-        title: "Why is it useful for your company?",
-        subtitle: "By handing over assignments to students for work, you free experienced employees from routine tasks, as well as increase brand awareness.",
-        topItemImage: resourceSaving,
-        topItemTitle: "Resource saving",
-        topItemSubtitle: "Delegate tasks for free and get results that can be used in your future work.",
-        leftItemImage: publicize,
-        leftItemTitle: "Publicize",
-        leftItemSubtitle: "We take pride in the real-world value of the tasks that our students perform and we share them in all available channels.",
-        rightItemImage: prBoost,
-        rightItemTitle: "PR boost",
-        rightItemSubtitle: "This will increase the visibility of the company among job seekers and attract those who share the values of your company."
-    }
     return (
         <ContentWhy content={contentWhyUseful} />
     )
