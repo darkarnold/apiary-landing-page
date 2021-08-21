ca624c668e7582de22810eb28a159a8d8301bb8e
SilentStorm <tongkorn.p@gmail.com>
2021-08-22 02:13:03 +0700

    Refactor: ContentWhy component

--- src/components/ContentWhy.js
+++ src/components/ContentWhy.js
@@ -1,31 +1,22 @@
 import React from "react";
 
 function contentWhy(props) {
-    console.log(props);
     return (
         <section className="content content_theme_dark">
             <div className="content__wrapper">
                 <h2 className="content__heading content__heading_theme_dark">{props.content.title}</h2>
                 <h3 className="content__text content__text_theme_dark">{props.content.subtitle}</h3>
                 <div className={`${props.whyChooseUs ? 'grid grid__items grid__items-why-choose-us' : 'grid grid__items'}`}>
-                    <div className="grid__item">
-                        <img src={`${props.content.topItem.image}`} alt={props.content.topItem.title} className="grid__item-image" />
-                        <h4 className="grid__title">{props.content.topItem.title}</h4>
-                        <p className="grid__subtitle">{props.content.topItem.subtitle}</p>
-                    </div>
-                    <div className="grid__item">
-                        <img src={`${props.content.leftItem.image}`} alt={props.content.leftItem.title} className="grid__item-image" />
-                        <h4 className="grid__title">{props.content.leftItem.title}</h4>
-                        <p className="grid__subtitle">{props.content.leftItem.subtitle}</p>
-                    </div>
-                    <div className="grid__item">
-                        <img src={`${props.content.rightItem.image}`} alt={props.content.rightItem.title} className="grid__item-image" />
-                        <h4 className="grid__title">{props.content.rightItem.title}</h4>
-                        <p className="grid__subtitle">{props.content.rightItem.subtitle}</p>
-                    </div>
+                    {props.content.items.map(item => (
+                        <div className="grid__item" key={item.position}>
+                            <img src={`${item.image}`} alt={item.title} className="grid__item-image" />
+                            <h4 className="grid__title">{item.title}</h4>
+                            <p className="grid__subtitle">{item.subtitle}</p>
+                        </div>
+                    ))}
                 </div>
             </div>
-        </section>
+        </section >
     )
 }
 
--- src/data.js
+++ src/data.js
@@ -8,39 +8,46 @@ import experience from "./images/experience.png";
 export const contentWhyUseful = {
     title: "Why is it useful for your company?",
     subtitle: "By handing over assignments to students for work, you free experienced employees from routine tasks, as well as increase brand awareness.",
-    topItem: {
+    items: [{
+        position: "1",
         image: resourceSaving,
         title: "Resource saving",
         subtitle: "Delegate tasks for free and get results that can be used in your future work.",
     },
-    leftItem: {
+    {
+        position: "2",
         image: publicize,
         title: "Publicize",
         subtitle: "We take pride in the real-world value of the tasks that our students perform and we share them in all available channels.",
     },
-    rightItem: {
+    {
+        position: "3",
         image: prBoost,
         title: "PR boost",
         subtitle: "This will increase the visibility of the company among job seekers and attract those who share the values of your company."
-    }
+    }]
 }
 
 export const contentWhyChooseUs = {
     title: "Why choose us?",
     subtitle: "We have built the largest IT company in Russia. We know how to develop cool employees, and we work hard to make our clients' lives better and happier.",
-    topItem: {
+    items: [{
+        position: "1",
         image: quality,
         title: "Quality",
         subtitle: "The Yandex search engine is the fourth ranked search engine in the world in terms of number of requests processed.",
     },
-    leftItem: {
+    {
+        position: "2",
+
         image: professionalism,
         title: "Professionalism",
         subtitle: "Practicum by Yandex is one of the most popular online education services in Russia, allowing students to get a new profession or master a new skill.",
     },
-    rightItem: {
+    {
+        position: "3",
         image: experience,
         title: "Experience",
         subtitle: "Our students are already working at Accenture, Tesla, Invitae. Our specialty is practical training for the real world."
-    }
+    }]
 }
