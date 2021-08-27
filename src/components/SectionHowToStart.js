32a02d9f05a13f6ab056eeda179d4d10bd84068a
SilentStorm <tongkorn.p@gmail.com>
2021-08-27 15:56:06 +0700

    Feat: Add section data,refactor

--- src/components/SectionHowToStart.js
+++ src/components/SectionHowToStart.js
@@ -1,47 +1,30 @@
 import React from "react";
-import checklist from "../images/checklist.svg";
-import question from "../images/question.svg";
-import work from "../images/work.svg";
+import { contentHowToStart } from "../data.js";
 
 function SectionHowToStart() {
     return (
         <section className="content">
             <div className="content__wrapper">
-                <h2 className="howToStart__heading">How to Start?</h2>
+                <h2 className="howToStart__heading">{contentHowToStart.title}</h2>
                 <div className="grid grid__items howToStart__grid-items">
-                    <div className="grid__item">
-                        <img src={checklist} alt="checklist" className="grid__item-image" />
-                        <p className="howToStart__text hide__device_minwidth_1024">1</p>
-                        <h4 className="howToStart__title">Fill in the checklist</h4>
-                        <p className="howToStart__subtitle">So that we can understand your request and how we can help you</p>
-                    </div>
-                    <div className="grid__item">
-                        <img src={question} alt="clarify question" className="grid__item-image" />
-                        <p className="howToStart__text hide__device_minwidth_1024">2</p>
-                        <h4 className="howToStart__title">We will clarify the details and connect the necessary experts</h4>
-                        <p className="howToStart__subtitle">We will help you formulate the request so that it's understandable to our students, and you will receive a high-quality solution in a timely manner</p>
-                    </div>
-                    <div className="grid__item">
-                        <img src={work} alt="working bee" className="grid__item-image" />
-                        <p className="howToStart__text hide__device_minwidth_1024">3</p>
-                        <h4 className="howToStart__title">We will start solving your tasks</h4>
-                        <p className="howToStart__subtitle">Practicum by Yandex students will solve the task, then our curators check and correct their solutions. We'll send you the work. Additionally, if you wish, you may talk to the authors of the best solutions and invite them to join you</p>
-                    </div>
+                    {contentHowToStart.items.map(item => (
+                        <div className="grid__item" key={item.position}>
+                            <img src={item.image} alt={item.title} className="grid__item-image" />
+                            <p className="howToStart__text hide__device_minwidth_1024">{item.position}</p>
+                            <h4 className="howToStart__title">{item.title}</h4>
+                            <p className="howToStart__subtitle">{item.subtitle}</p>
+                        </div>
+                    ))}
                     <div className="grid__item steps-wrapper">
                         <div className="steps">
-                            <div className="step">
-                                <p className="howToStart__text">1</p>
-                            </div>
-                            <div className="step">
-                                <p className="howToStart__text">2</p>
-                            </div>
-                            <div className="step">
-                                <p className="howToStart__text">3</p>
-                            </div>
+                            {contentHowToStart.items.map(item => (
+                                <div className="step" key={item.position}>
+                                    <p className="howToStart__text">{item.position}</p>
+                                </div>
+                            ))}
                         </div>
                     </div>
                 </div>
-
             </div>
         </section>
     )
--- src/data.js
+++ src/data.js
@@ -5,6 +5,9 @@ import quality from "./images/quality.png";
 import professionalism from "./images/Professionalism.png";
 import experience from "./images/experience.png";
 import peopleWithLaptop from "./images/peopleWithLaptop.png";
+import checklist from "./images/checklist.svg";
+import question from "./images/question.svg";
+import work from "./images/work.svg";
 
 export const contentWhyUseful = {
   title: "Why is it useful for your company?",
@@ -71,3 +74,27 @@ export const contentDelegateTasks = {
     "Check out the projects our students have done for companies in the USA and beyond",
   image: peopleWithLaptop,
 };
+
+export const contentHowToStart = {
+  title: "How to Start?",
+  items: [
+    {
+      position: "1",
+      image: checklist,
+      title: "Fill in the checklist",
+      subtitle: "So that we can understand your request and how we can help you"
+    },
+    {
+      position: "2",
+      image: question,
+      title: "We will clarify the details and connect the necessary experts",
+      subtitle: "We will help you formulate the request so that it's understandable to our students, and you will receive a high-quality solution in a timely manner"
+    },
+    {
+      position: "3",
+      image: work,
+      title: "We will start solving your tasks",
+      subtitle: "Practicum by Yandex students will solve the task, then our curators check and correct their solutions. We'll send you the work. Additionally, if you wish, you may talk to the authors of the best solutions and invite them to join you"
+    },
+  ]
+}
