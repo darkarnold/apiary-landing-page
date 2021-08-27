7c8b1d818a5e0102be9a329ae1685b76f53a1718
darkarnold <arnoldndiwalana@gmail.com>
2021-08-27 18:15:45 +0300

    fix: made changes to heaader section and the mobile layout

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,25 +1,32 @@
 import React from "react";
-import Logo from "./Logo";
-import Button from "./Button";
+import logoY from "../images/logo-y.svg";
+import logoPracticum from "../images/logo-practicum.png";
 
 function Header() {
   return (
-    <nav className="header">
-      <Logo />
-      <ul className="header__menu">
-        <li className="header__menu-items">We can help!</li>
-        <li className="header__menu-items">About</li>
-        <li className="header__menu-items">How to start</li>
-        <li className="header__menu-items">Projects</li>
-        <li className="header__menu-items">Contacts</li>
-      </ul>
-      <Button />
-
-      <div className="header__hamburger">
-        <span className="header__hamburger-bars"></span>
-        <span className="header__hamburger-bars"></span>
+    <header className="header">
+      <div className="header__content-wrapper">
+        <div className="logo">
+          <img src={logoY} alt="Yandex logo" className="logo__yandex" />
+          <img
+            src={logoPracticum}
+            alt="Practicum logo"
+            className="logo__practicum"
+          />
+        </div>
+        <button className="button__hamburger"></button>
+        <nav>
+          <ul className="header__menu">
+            <li className="header__menu-items">We can help!</li>
+            <li className="header__menu-items">About</li>
+            <li className="header__menu-items">How to start</li>
+            <li className="header__menu-items">Projects</li>
+            <li className="header__menu-items">Contacts</li>
+          </ul>
+        </nav>
+        <button className="button">Delegate a task</button>
       </div>
-    </nav>
+    </header>
   );
 }
 
